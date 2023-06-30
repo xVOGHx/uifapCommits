fc6e6477632ea0f4101e8a11690856b81e8928eb
xVOGHx <guilhermefortescoelho@gmail.com>
2023-06-30 12:21:18 -0300

    first commit

---
+++ exercicio 008.js
@@ -0,0 +1,23 @@
+let operação = "SOMA";
+    operação = operação.toLowerCase();
+let n1 = "GUILHERME"
+let n2 = " FORTES"
+
+switch (operação){
+    case "soma":
+        console.log (`a conta é ${n1} + ${n2} = ${n1 + n2}`);
+        break;
+    case "subtração":
+            console.log(`a conta é ${n1} - ${n2} = ${n1 - n2}`);
+            break;
+    case "adição":
+        let adição = n1 * n2
+        console.log(` conta é ${n1} * ${n2} = ${adição}`);
+        break;
+    case "divisão":
+        let divisão = n1 / n2
+        console.log(` conta é ${n1} ÷ ${n2} = ${divisão}`);
+        break;
+    default:
+        console.log(`operação invalida`)
+}
---
+++ exercicio 009.js
@@ -0,0 +1,9 @@
+let n1 = 1
+let n2 = 25
+
+if(n1>n2){
+    console.log (`o maior numero é ${n1}`)
+}
+else{
+    console.log (`o maior numero é ${n2}`)
+}
