f948d0e534713105c87eb14d6e318d9df486af9b
xVOGHx <guilhermefortescoelho@gmail.com>
2023-07-07 10:01:59 -0300

    segundo commit

---
+++ exercicios/exc 011.js
@@ -0,0 +1,9 @@
+let senha = "1234"
+
+if(senha === 1234){
+    console.log (`ACESSO PERMITIDO`)
+}
+
+else {
+    console.log (`ACESSO NEGADO`)
+}
---
+++ exercicios/exc 012.js
@@ -0,0 +1,15 @@
+let n1 = 10
+let n2 = 5
+let media = (n1 + n2) /2
+
+console.log (media)
+
+if (media >= 7){
+    console.log(`Aprovado`)
+    if (media === 10){
+        console.log (`Aprovado com Distinção`)
+    }}
+
+else {
+    console.log(`Reprovado`)
+}
