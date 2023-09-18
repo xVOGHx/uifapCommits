739f7fcf6d45bc94e0144f1232506f95871ec5e5
Eric <guilhermefortescoelho@gmail.com>
2023-09-18 11:48:39 -0300

    minha meta é ter um github bonito

--- modulo_02_reborn/1509/src/controllers/usuarioController.js
+++ modulo_02_reborn/1509/src/controllers/usuarioController.js
@@ -24,5 +24,73 @@ module.exports = {
         } catch (error) {
             res.json({error})
         }
+    },
+
+    async buscarUsuario(req, res){
+        const { id } = req.params;
+    
+        try {
+            const usuario = await prisma.usuario.findUnique({
+                where: { id: parseInt(id, 10) },
+            })
+            res.json(usuario)
+        }   catch (error){
+            res.json(error)
+        }
+    },
+
+    async buscarUsuarios(req, res) {
+        const usuarios = await prisma.usuario.findMany()
+        res.json(usuarios)
+    },
+
+    async atualizaUsuario(req, res) {
+        try {
+            const { id } = req.params;
+            const { nome, email} = req.body;
+
+            const usuario = await prisma.usuario.findUnique({
+                where: { id: parseInt(id) },
+            });
+            if(!usuario) {
+                return res.json({ error: "Usuario não existe"});
+            }
+
+            usuario = await prisma.usuario.update({
+                where: { id: parseInt(id) },
+                data: { nome, email },
+                select: {
+                    nome: true,
+                    email: true
+                }
+            });
+            return res.json(usuario);
+
+        } catch (error) {
+            res.json(error)
+        }
+    },
+
+    async deletarUsuario(req, res) {
+        try {
+            const { id } = req.params;
+    
+            const usuario = await prisma.usuario.findUnique({
+                where: { id: parseInt(id)},
+            });
+    
+            if (!usuario){
+                return res.json({ error: "Usuario não existe"})
+            }else{
+                usuario = await prisma.usuario.delete({
+                    where: { id: parseInt(id) }
+                })
+                return res.json("Usuario Deletado")
+            }
+            
+        } catch (error) {
+            res.json(error)
+        }
     }
 }
+
--- modulo_02_reborn/1509/src/routes.js
+++ modulo_02_reborn/1509/src/routes.js
@@ -3,5 +3,9 @@ const usuarioController = require("./controllers/usuarioController");
 const router = Router();
 
 router.post("/criarUsuario", usuarioController.criaUsuario);
+router.get(`/buscarUsuario/:id`, usuarioController.buscarUsuario)
+router.get(`/buscarUsuario`, usuarioController.buscarUsuarios);
+router.put(`/atualizaUsuario/:id`, usuarioController.atualizaUsuario);
+router.delete(`/deletarUsuario/:id`, usuarioController.deletarUsuario);
 
 module.exports = { router }
