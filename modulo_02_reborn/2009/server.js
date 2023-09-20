19d834027071a3faa83453336866e7c4ad4f2bac
Eric <guilhermefortescoelho@gmail.com>
2023-09-20 10:40:25 -0300

    segundo do dia

--- modulo_02_reborn/2009/server.js
+++ modulo_02_reborn/2009/server.js
@@ -3,14 +3,25 @@ const {PrismaClient} = require("@prisma/client");
 const coockieParser = require("cookie-parser");
 const bcrypt = require("bcrypt");
 
-const app = express()
+const app = express();
 
 app.use(express.json());
 const prisma = new PrismaClient();
 
 app.post("/registar", async (req, res) => {
     const {nome, senha} = req.body;
-    res,json(nome,senha)
+    await bcrypt.hash(senha, 10).then((hash) => {
+        prisma.usuario.create({
+            data: {
+                nome,
+                senha: hash
+            }
+        }).then(() => {
+            res.json("Usuario criado");
+        }).catch( (err) => {
+            res.json({err: "Algo deu errado"});
+        })
+    })
 })
 
 app.post("/login", async (req, res) => {
